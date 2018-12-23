58f06d0e111106ce705b1efd47dde458d8134b09
feilong <feilongphone@gmail.com>
2018-12-23 17:22:22 +0000

    add report

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,36 @@
 <template>
   <section class="comic">
-    <b-navbar 
-      id="rss-nav" 
-      toggleable="md" 
-      type="dark" 
+    <b-navbar
+      id="rss-nav"
+      toggleable="md"
+      type="dark"
       variant="info">
       <b-navbar-toggle target="nav_collapse"/>
 
       <div>
-        <b-navbar-brand 
-          v-if="loading" 
+        <b-navbar-brand
+          v-if="loading"
           @click="update()">
           <div class="loader-5"/>
         </b-navbar-brand>
-        <b-navbar-brand 
-          v-else 
-          @click="update()">{{ this.$store.state.data.title }}</b-navbar-brand>
+        <b-navbar-brand
+          v-else
+          @click="update()">{{ this.$store.state.data.title }}
+        </b-navbar-brand>
       </div>
-      <b-collapse 
-        id="nav_collapse" 
+      <b-collapse
+        id="nav_collapse"
         is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="https://github.com/feilongfl/Cimoc">
             <b-img
-              src="https://camo.githubusercontent.com/69b991d1e0f31d6bb3e984495ca0cdb21ac41d38/68747470733a2f2f696d672e736869656c64732e696f2f6769746875622f72656c656173652f6665696c6f6e67666c2f43696d6f632e737667"/>
+              src="https://camo.githubusercontent.com/69b991d1e0f31d6bb3e984495ca0cdb21ac41d38/68747470733a2f2f696d672e736869656c64732e696f2f6769746875622f72656c656173652f6665696c6f6e67666c2f43696d6f632e737667"></b-img>
           </b-nav-item>
 
           <!--<b-nav-item href="#">{{ this.$store.state.pubDate }}</b-nav-item>-->
-          <b-nav-item-dropdown 
-            text="IPFS Server" 
+          <b-nav-item-dropdown
+            text="IPFS Server"
             right>
             <b-dropdown-item @click="changeIpfsServer('https://feilong.ml/comic.json')">Cloudflare</b-dropdown-item>
             <b-dropdown-item @click="changeIpfsServer('https://ipfs.io/ipns/feilong.ml/comic.json')">ipfs.io
@@ -38,17 +39,23 @@
             </b-dropdown-item>
           </b-nav-item-dropdown>
           <!--<b-nav-item href="#">{{ data.description }}</b-nav-item>-->
-        </b-navbar-nav>
+          <b-nav-item right href="https://github.com/feilongfl/rss2web/issues">
+            反馈
+          </b-nav-item>
+          <b-nav-item right href="https://feilong.home.blog">
+            feilong 2018
+          </b-nav-item>
+          </b-navbar-nav>
       </b-collapse>
     </b-navbar>
 
-    <b-alert 
+    <b-alert
       :show="alertBar.Time"
       :variant="alertBar.Level"
       @dismissed="alertBar.Time=0"
       @dismiss-count-down="(t) => {alertBar._time = t}">
       <p>{{ alertBar.Message }}</p>
-      <b-progress 
+      <b-progress
         v-if="!(alertBar.Time in [0,1])"
         :max="alertBar.Time"
         :value="alertBar._time"
@@ -58,53 +65,60 @@
 
     <div id="rss-body">
       <b-list-group>
-        <div 
-          v-for="(item, index) in this.$store.state.data.items" 
+        <div
+          v-for="(item, index) in this.$store.state.data.items"
           :key="index">
-          <b-list-group-item 
+          <b-list-group-item
             :href="item.link"
             class="d-flex justify-content-between align-items-center"
             @click="markRead(index)">
             <b-media>
               <!--<b-img slot="aside" height="240" width="160" alt="placeholder" :src="item.cover" />-->
-              <b-img-lazy 
-                slot="aside" 
-                :src="item.cover" 
-                rounded 
-                height="200" 
-                width="160" 
+              <b-img-lazy
+                slot="aside"
+                :src="item.cover"
+                rounded
+                height="200"
+                width="160"
                 alt="img"
                 blank-src="/icon.png"/>
               <h5 class="mt-0">{{ item.title }}</h5>
-              <b-badge 
-                v-if="item.readed" 
-                variant="secondary" 
-                pill>Readed</b-badge>
+              <b-badge
+                v-if="item.readed"
+                variant="secondary"
+                pill>Readed
+              </b-badge>
 
-              <b-badge 
-                v-if="item.category === 'manhuagui'" 
-                variant="primary" 
-                pill>{{ item.category }}</b-badge>
-              <b-badge 
-                v-else-if="item.category === '57comic'" 
-                variant="success" 
-                pill>{{ item.category }}</b-badge>
-              <b-badge 
-                v-else-if="item.category === 'tencent'" 
-                variant="warning" 
-                pill>{{ item.category }}</b-badge>
-              <b-badge 
-                v-else-if="item.category === 'pufei'" 
-                variant="danger" 
-                pill>{{ item.category }}</b-badge>
-              <b-badge 
-                v-else-if="item.category === 'webtoons'" 
-                variant="primary" 
-                pill>{{ item.category }}</b-badge>
-              <b-badge 
-                v-else 
-                variant="secondary" 
-                pill>{{ item.category }}</b-badge>
+              <b-badge
+                v-if="item.category === 'manhuagui'"
+                variant="primary"
+                pill>{{ item.category }}
+              </b-badge>
+              <b-badge
+                v-else-if="item.category === '57comic'"
+                variant="success"
+                pill>{{ item.category }}
+              </b-badge>
+              <b-badge
+                v-else-if="item.category === 'tencent'"
+                variant="warning"
+                pill>{{ item.category }}
+              </b-badge>
+              <b-badge
+                v-else-if="item.category === 'pufei'"
+                variant="danger"
+                pill>{{ item.category }}
+              </b-badge>
+              <b-badge
+                v-else-if="item.category === 'webtoons'"
+                variant="primary"
+                pill>{{ item.category }}
+              </b-badge>
+              <b-badge
+                v-else
+                variant="secondary"
+                pill>{{ item.category }}
+              </b-badge>
               <p>
                 {{ item.pubDate }}
                 <!--{{item.description}}-->
@@ -114,7 +128,7 @@
           </b-list-group-item>
         </div>
         <div>
-          <b-list-group-item 
+          <b-list-group-item
             class="d-flex justify-content-between align-items-center"
             @click="update()">
             <b-media>
